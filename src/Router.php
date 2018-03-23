0dbe86088c58f1592c92b81cf195f7e01c5dce95
Julien <jujubouhouch@live.fr>
2018-03-23 20:59:19 +0100

    Mise en place MVCR + test URL

--- src/Router.php
+++ src/Router.php
@@ -6,11 +6,44 @@
  * Time: 17:51
  */
 
+require_once("view/View.php");
+require_once("control/Controller.php");
+
 class Router
 {
-    public function main() {
+    /*
+    public function __construct(SerieStorage $seriedb) {
+        $this->seriedb = $seriedb;
+    }*/
 
-        echo 'bonjour';
+    public function main() {
+        $view = new View($this);
+        $ctrl = new Controller($view);
+
+
+        $mangaId = key_exists('manga', $_GET)? $_GET['manga']: null;
+        $action = key_exists('action', $_GET)? $_GET['action']: null;
+
+        if ($action === null) {
+            /* Pas d'action demandée : par défaut on affiche
+               * la page d'accueil, sauf si une couleur est demandée,
+               * auquel cas on affiche sa page. */
+            $action = ($mangaId === null)? "accueil": "voir";
+        }
+
+        if($action != null){
+            switch ($action) {
+                case "voir":
+                    $ctrl->mangaPage($mangaId);
+
+            }
+
+        }
+
+
+        //$view->makeTestPage();
+        //$view->makeMangaPage();
+        $view->render();
 
     }
 }
---
+++ src/view/templateSerie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title><?php echo $stitre ?></title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="" />
+</head>
+<body>
+
+<main>
+    <h1><?php echo $stitre; ?></h1>
+    <h1><?php echo $sauteur; ?></h1>
+
+</main>
+
+</body>
+</html>
