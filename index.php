5cea1318a0cd118f4b654ebb2659815da08c82a7
slogix <slogi@live.com>
2018-03-24 11:59:05 +0100

    Index

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ require_once("model/SerieStorageImpl.php");
 require_once("Connexion.php");
 require_once("Router.php");
 
+<<<<<<< HEAD
 $database = new Connexion();
 $db = $database->getConnection();
 
 $r = new Router( new SerieStorageImpl($db));
 
+$r->main();
 =======
 $router = new Router();
 $router->main();
