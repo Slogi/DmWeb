7bcf000ee41815b5359d5f90628ae87536ab05a2
slogix <slogi@live.com>
2018-03-24 20:44:25 +0100

    ROUTEUR

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once("model/SerieStorageImpl.php");
 require_once("Connexion.php");
 require_once("Router.php");
 
-<<<<<<< HEAD
 $database = new Connexion();
 $db = $database->getConnection();
 
@@ -14,14 +13,5 @@ $r = new Router( new SerieStorageImpl($db, new MangaStorageImpl($db)));
 
 $r->main();
 
-=======
-
-$database = new Connexion();
-$db = $database->getConnection();
-
-$r = new Router($db);
-
-$r->main();
 
 
->>>>>>> master
