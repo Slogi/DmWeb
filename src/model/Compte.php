43a1106fb4c95d06a1de255d575c491cb14e465d
slogix <slogi@live.com>
2018-03-26 17:33:31 +0100

    Changement classe compte

--- src/model/Compte.php
+++ src/model/Compte.php
@@ -5,6 +5,10 @@ class Compte
 {
     protected $pseudo;
     protected $mdp;
+    protected $nom;
+    protected $prenom;
+    protected $dateBirth;
+    protected $genre;
     protected $statut;
 
     public function __construct($pseudo, $mdp)
